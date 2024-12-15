791378e23629e9cf8396ca4fa776970cb203b2da
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2024-12-15 21:21:57 +0100

    feat: Refactor App component to use new section components for better modularity and maintainability

---
+++ src/sections/index.ts
@@ -0,0 +1,19 @@
+import CustomerReviews from "./customer-reviews";
+import Footer from "./footer";
+import Hero from "./hero";
+import PopularProducts from "./popular-products";
+import Services from "./services";
+import SpecialOffers from "./special-offers";
+import Subscribe from "./subscribe";
+import SuperQuality from "./super-quality";
+
+export {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffers,
+  Subscribe,
+  SuperQuality,
+};
